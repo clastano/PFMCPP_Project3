52fb89423e18beac9d0e750e3d36ac2b74ae007e
clastano <poke.clastano@gmail.com>
2025-12-02 17:54:08 +0000

    Added the 10th object that is made of 5 sub-objects

--- main.cpp
+++ main.cpp
@@ -337,17 +337,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) Synthesizer
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) Manufacturer
+    2) SynthEngine
+    3) Case
+    4) Distributor
+    5) InternalSequencer
 3 things it can do:
-    1)
-    2)
-    3)
+    1) play notes
+    2) change internal status
+    3) light-up feedback LEDs
  */
 
  /*
